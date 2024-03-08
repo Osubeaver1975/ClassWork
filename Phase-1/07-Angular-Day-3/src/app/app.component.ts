64ad5bf09d07afd495517fff9650780a8b54dd49
Justin Durow <justindurow@gmail.com>
2024-03-08 02:07:36 +0000

    prior to class on 3.7.24

--- Phase-1/07-Angular-Day-3/src/app/app.component.ts
+++ Phase-1/07-Angular-Day-3/src/app/app.component.ts
@@ -1,14 +1,20 @@
+// import gives us access to external features and components
+// most imported names must be included in the imports property of the component
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SampleFormComponent } from './sample-form/sample-form.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, SampleFormComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // this is what is used to includ this 
+                        // component on a page
+  standalone: true,   // added in angular 17 to allow components to be independent
+  imports: [RouterOutlet, SampleFormComponent], //identifies things used
+  templateUrl: './app.component.html', // where to find the html file
+  styleUrl: './app.component.css'     // where to find the css
 })
+// this defines a module for this component
+// export - says let things outside this component access it
+// class- this is a group of things associated with this component
 export class AppComponent {
   pageTitle = 'Welcome to FSD - Cohort-1-2024 Angular App';
 }
--- Phase-1/07-Angular-Day-3/src/app/sample-form/sample-form.component.ts
+++ Phase-1/07-Angular-Day-3/src/app/sample-form/sample-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// FormesModule is required if component uses html forms
 import { FormsModule } from '@angular/forms'
 
 @Component({
-  selector: 'sample-form',
+  selector: 'sample-form', // this is the tag used in any page to include this form we created
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './sample-form.component.html',
@@ -11,7 +12,15 @@ import { FormsModule } from '@angular/forms'
 })
 export class SampleFormComponent {
   
-    onSubmit() {
+    
+  
+  
+          // a method to process any data accessible to the component
+          // public says this variable may be accessed by anything using this component
+          // public --> access modifier --> is used to define data in a module instead of let/ var
+          // private --> access modifier --> is another access modifier that limits access to code in this module
+          // public allows angular to access the data
+  onSubmit() {
       console.log(`-----------------------------------------------`)
       console.log(` First name: ${this.fname}`)
       console.log(`  Last name: ${this.lname}`)
@@ -21,6 +30,10 @@ export class SampleFormComponent {
       console.log(`allow email: ${this.email_preference}`)
     }
 
+
+          // data used by the component
+
+          // these variables are connected to input controls in a from via [(NgModule)]
       public fname = "";
       public lname = "";
       public birthDate = "";
